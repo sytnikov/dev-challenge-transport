03c8569ad49cfd38c1787a9b87d09753a969b4d8
sytnikov <sytnikov.alexey@gmail.com>
2024-06-15 18:28:48 +0300

    env example variables, server, app separated

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import "dotenv/config"
+
+import "./mqttClient";
+import app from "./app"
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
